61b208f409f0a53221592c11a907806b3d57de4d
Liam Deman <liam.deman@vbklogisticsautomation.be>
2022-08-25 16:08:36 +0200

    binary search and sequential search

---
+++ binarySearchArray.py
@@ -0,0 +1,38 @@
+#binary search array with iterator
+def binarySearch(target, sortedArray):
+    l = 0
+    r = len(sortedArray) -1
+    while l != r :
+        m = int((l+r)/2)
+        if sortedArray[m] < target:
+            l = m +1
+        else:
+            r = m
+
+    if sortedArray[l] == target:
+        foundIndex = l
+    else:
+        foundIndex = -1
+
+    return foundIndex
+
+#with recursion
+def binarySearchRecursive(target, sortedArray):
+    return recursion(target, sortedArray, 0, len(sortedArray) -1)
+
+def recursion(target, sortedArray, l, r):
+    if l == r:
+        if target == sortedArray[l]:
+            return l
+        else:
+            return -1
+
+    else:
+        m = int((l+r)/2)
+        if sortedArray[m] < target :
+            return recursion(target, sortedArray, m+1, r)
+        else:
+            return recursion(target, sortedArray, l, m)
+
+
+print(binarySearchRecursive(2, [0,1,1,1,1,2,3,4,5,6]))
---
+++ sequentialSearchArray.py
@@ -0,0 +1,21 @@
+#sequential search array
+def seqSearch(item, list):
+    #set index
+    i = 0
+    #stop when index passed last item of list
+    #or when item found at index
+    while (i < len(list) and list[i]!= item):
+        #not found -> move index 1 position
+        i += 1
+    
+    #index didn't pass last item of list? index found, return it
+    #else notfound, retourn -1
+    if i < len(list):
+        index = i
+    else:
+        index = -1
+
+    return index
+
+
+print(seqSearch(4, [1,2,3,4,5,6,7]))
